e79d314174cd19041f621338063fbc3efabff562
marcocnc <marcocancelliere56@gmail.com>
2024-09-21 20:01:10 +0200

    chore: imported splide in main jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import '@splidejs/react-splide/css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
