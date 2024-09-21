14189aa71850040578fa13db8133cc3bfcf1382a
marcocnc <marcocancelliere56@gmail.com>
2024-09-21 19:22:11 +0200

    refactor: dinamic print of nav items with hooks

--- src/components/partials/Navbar/Navbar.jsx
+++ src/components/partials/Navbar/Navbar.jsx
@@ -1,7 +1,39 @@
+import { useState, useEffect } from 'react';
 import logo from '../../../assets/img/logo.jpg';
 import '../../../scss/partials/Header/header.scss';
 
 const Navbar = () => {
+
+    const navItems = [
+        {
+            href: '#',
+            item: 'Home'
+        },
+        {
+            href: '#',
+            item: 'Chi siamo'
+        },
+        {
+            href: '#',
+            item: 'I nostri marchi'
+        },
+        {
+            href: '#',
+            item: 'Dove siamo'
+        },
+        {
+            href: '#',
+            item: 'Contatti'
+        },
+    ]
+
+    const [navBarArr, setNavBarArr] = useState([]);
+
+    useEffect(() => {
+        setNavBarArr(navItems);
+    }, []);
+    
+
     return (
         <header id="main-header">
             <div className="container">
@@ -15,35 +47,20 @@ const Navbar = () => {
                       </button>
                       <div className="collapse navbar-collapse ms-4" id="navbarNavDropdown">
                         <ul className="navbar-nav d-flex justify-content-between w-100">
-                          <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">
-                                Home
-                            </a>
-                          </li>
-    
-                          <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Chi siamo
-                            </a>
-                          </li>
-    
-                          <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                I nostri marchi
-                            </a>
-                          </li>
-    
-                          <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Dove siamo
-                            </a>
-                          </li>
-    
-                          <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Contatti
-                            </a>
-                          </li>
+                            {
+                                navBarArr && 
+                                navBarArr.length > 0 &&
+                                navBarArr.map((listItem, index) => {
+                                    const {href, item} = listItem;
+                                    return (
+                                        <li key={index} className="nav-item">
+                                            <a className='nav-link' href={href}>
+                                                {item}
+                                            </a>
+                                        </li>
+                                    )
+                                })
+                            }
                         </ul>
                       </div>
                     </div>
