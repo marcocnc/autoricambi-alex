0e4f649fe2046e80ff90629a06d9057f9481a34a
marcocnc <marcocancelliere56@gmail.com>
2024-09-21 15:35:11 +0200

    chore: installed bootstrap, new font and font awesome

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
 import './scss/vars/vars.scss';
 import './scss/index.scss';
