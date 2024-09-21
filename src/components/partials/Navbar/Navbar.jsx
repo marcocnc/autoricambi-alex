040e49736489457c10355e858c646b1bcca940d6
marcocnc <marcocancelliere56@gmail.com>
2024-09-21 19:31:27 +0200

    refactor: generic refactoring

--- src/components/partials/Navbar/Navbar.jsx
+++ src/components/partials/Navbar/Navbar.jsx
@@ -39,34 +39,42 @@ const Navbar = () => {
             <div className="container">
                 <nav className="navbar navbar-expand-lg">
                     <div className="container-fluid">
-                      <a className="navbar-brand" href="#">
-                        <img className="logo" src={logo} alt="logo"/>
-                      </a>
-                      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                      </button>
-                      <div className="collapse navbar-collapse ms-4" id="navbarNavDropdown">
-                        <ul className="navbar-nav d-flex justify-content-between w-100">
-                            {
-                                navBarArr && 
-                                navBarArr.length > 0 &&
-                                navBarArr.map((listItem, index) => {
-                                    const {href, item} = listItem;
-                                    return (
-                                        <li key={index} className="nav-item">
-                                            <a className='nav-link' href={href}>
-                                                {item}
-                                            </a>
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
-                      </div>
+                        <a className="navbar-brand" href="#">
+                            <img className="logo" src={logo} alt="logo"/>
+                        </a>
+                        <button 
+                            className="navbar-toggler" 
+                            type="button" 
+                            data-bs-toggle="collapse" 
+                            data-bs-target="#navbarNavDropdown" 
+                            aria-controls="navbarNavDropdown" 
+                            aria-expanded="false" 
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse ms-4" id="navbarNavDropdown">
+                            <ul className="navbar-nav d-flex justify-content-between w-100">
+                                {
+                                    navBarArr && 
+                                    navBarArr.length > 0 &&
+                                    navBarArr.map((listItem, index) => {
+                                        const {href, item} = listItem;
+                                        return (
+                                            <li key={index} className="nav-item">
+                                                <a className='nav-link' href={href}>
+                                                    {item}
+                                                </a>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
                     </div>
-                  </nav>
+                </nav>
             </div>
-         </header>
+        </header>
     )
 }
 
