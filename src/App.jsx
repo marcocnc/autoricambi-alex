5c1da902edcb99788ce6ce71410495df6460aecc
marcocnc <marcocancelliere56@gmail.com>
2024-09-21 15:05:41 +0200

    feat: initialized app scafolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Header from "./components/structure/Header.jsx";
+import Main from "./components/structure/Main.jsx";
+import Footer from "./components/structure/Footer.jsx";
+
 const App = () => {
-  return (
-	<div>App</div>
-  )
+
+    return (
+        <div className="main-wrapper">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    )
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './scss/vars/vars.scss';
+import './scss/index.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
