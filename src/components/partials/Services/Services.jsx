16ee1c74fad2338a0acbfe1c6ec6c56806ce7a8d
marcocnc <marcocancelliere56@gmail.com>
2024-09-28 20:28:10 +0200

    refactor: removed modal

--- src/components/partials/Services/Services.jsx
+++ src/components/partials/Services/Services.jsx
@@ -13,7 +13,7 @@ const Services = () => {
         },
         {
             icon:'fa-wrench',
-            name: 'Sostituzione materiale difettoso'
+            name: 'Sostituzione materiale difettoso',              
         },
         {
             icon:'fa-car-battery',
@@ -50,37 +50,14 @@ const Services = () => {
                         const {icon, name} = service;
 
                         return (
-
-                            <>
-                                <div className="col-12 col-lg-6 service" key={index} data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    <i className={("fa-solid m-3 " + icon)}></i>
-                                    <span> {name} </span>
-                                </div>
-                            
-                                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div className="modal-dialog">
-                                        <div className="modal-content">
-                                        <div className="modal-header">
-                                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div className="modal-body">
-                                            ...
-                                        </div>
-                                        <div className="modal-footer">
-                                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                            <button type="button" className="btn btn-primary">Save changes</button>
-                                        </div>
-                                        </div>
-                                    </div>
-                                    </div>
-                            </>
-                            
+                            <div className="col-12 col-lg-6 service" key={index}>
+                                <i className={("fa-solid m-3 " + icon)}></i>
+                                <span> {name} </span>
+                            </div>
                         )
                     })
                 }
             </div>
-
           
             <div className="whatsapp-container mt-5">
                 <a href="#" className="btn whatsapp-btn">
