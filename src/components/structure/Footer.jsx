cac26a2737e27250aabce80d3417ae84c341c690
marcocnc <marcocancelliere56@gmail.com>
2024-10-19 16:47:17 +0200

    feat: footer first draft

--- src/components/structure/Footer.jsx
+++ src/components/structure/Footer.jsx
@@ -1,6 +1,22 @@
+import logo from '../../assets/img/logo.jpg';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <section className="container">
+            <div className="footer-vertical-col">
+                <div className="footer-logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="footer-info">
+                    <h2>Accessori e Ricambi Auto Alex</h2>
+                    <p>Via Giacomo Leopardi 57-59</p>
+                    <p>Volla (NA)</p>
+                </div>
+            </div>
+        </section>
+      
+    </footer>
   )
 }
 
